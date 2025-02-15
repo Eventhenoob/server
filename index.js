f723ea00f8250082dcde484dd6ee215111768ddf
Bharat Gawar <cbhrat50@gmail.com>
2025-02-15 10:18:55 +0530

    fix cors

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./config/config");
-const router = require("./routes/qart/index")
+const router = require("./routes/qart/index");
 const passport = require("passport");
 const { jwtStrategy } = require("./config/passport");
-const app = express()
-mongoose.connect(config.mongoose.url).then(()=>{
-    console.log("connect to mongodb")
-})
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(passport.initialize())
-passport.use("jwt",jwtStrategy)
-app.use("/verse",router)
+const cors = require("cors");
 
-app.get("/",(req,res)=>{
-    res.send("Hello welcome to Cart Project")
-})
-app.listen(config.port,()=>{
-    console.log("listening to port 8082")
-})
+const app = express();
+app.use(cors());
+mongoose.connect(config.mongoose.url).then(() => {
+  console.log("connect to mongodb");
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+passport.use("jwt", jwtStrategy);
+app.use("/verse", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello welcome to Cart Project");
+});
+app.listen(config.port, () => {
+  console.log("listening to port 8082");
+});
